f23f0da8329150359b35fdc478e7e3f9bec290ac
Carlos Nayan <carlosnayan@gmail.com>
2023-02-10 14:24:00 -0300

    indentado para deploy

--- src/pages/Contatos/Contatos.tsx
+++ src/pages/Contatos/Contatos.tsx
@@ -6,14 +6,14 @@ function Contatos() {
     return (
       <div className='pagecontatos'>
         <div className="contatos">
-        <h1 className='titleContatos'>Contatos</h1>
-        <ul className='buttonsContatos'>
-          <a  href="https://github.com/CarlosNayan" target="_blank"><button className='github'> <BsGithub/>  Github</button></a>
-          <a  href="https://www.linkedin.com/in/carlos-nayan-505298183/" target="_blank"><button className='linkedin'> <BsLinkedin/> Linkedin</button></a>
-          <a href="https://www.instagram.com/carls_nayan/" target="_blank"><button className='instagram'> <BsInstagram/> Instagram</button></a>
-          <a href="https://wa.me/+5591991869898" target="_blank"><button className='telefone'> <BsWhatsapp/> Whatsapp</button></a>
-          <a href="mailto:carlosnayan@gmail.com" target="_blank"><button className='email'> <MdEmail/> Email</button></a>
-        </ul>
+          <h1 className='titleContatos'>Contatos</h1>
+          <ul className='buttonsContatos'>
+            <a  href="https://github.com/CarlosNayan" target="_blank"><button className='github'> <BsGithub/>  Github</button></a>
+            <a  href="https://www.linkedin.com/in/carlos-nayan-505298183/" target="_blank"><button className='linkedin'> <BsLinkedin/> Linkedin</button></a>
+            <a href="https://www.instagram.com/carls_nayan/" target="_blank"><button className='instagram'> <BsInstagram/> Instagram</button></a>
+            <a href="https://wa.me/+5591991869898" target="_blank"><button className='telefone'> <BsWhatsapp/> Whatsapp</button></a>
+            <a href="mailto:carlosnayan@gmail.com" target="_blank"><button className='email'> <MdEmail/> Email</button></a>
+          </ul>
         </div>
       </div>
     );
