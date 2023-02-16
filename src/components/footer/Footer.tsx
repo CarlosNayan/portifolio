d5ed98cac6931c2b05235bee4c357a7648dfb061
Carlos Nayan <carlosnayan@gmail.com>
2023-02-16 19:41:02 -0300

    atualizado componente do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import './footer.css'
 export function Footer () {
     return(
         <div className="footer">
-            <p className="footertext">Site ainda em desenvolvimento utilizando ReactJS. Não possui suporte total para dispositivos móveis.</p>
+            <p className="footertext">Site ainda em desenvolvimento utilizando ReactJS. Otimizado para dispositivos móveis, mas para uma melhor experiência acesse em um desktop.</p>
         </div>
     )
 }
